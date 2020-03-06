6d248b818e0ed063db688b8014e1219303be6659
Anchit Gupta <guptaanchit29@gmail.com>
2020-03-06 17:41:40 +0530

    preprocess changes

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,6 +8,25 @@ import time
 DATA_PATH = '../ArrowDataAll/Train'
 THRESHOLD = 1000
 STRIDE = 3
+PATCH_IDX = []
+DICT = {}
+
+def generate_patches():
+    patch_x, patch_y = [], []
+    cnt = 0
+    for y in range(0,552-4,3):
+        for x in range(0,982-4,3):
+            cnt += 1
+            yy, xx = np.meshgrid(np.arange(x,x+4),np.arange(y,y+4))
+            idx_x = xx.reshape(16)
+            idx_y = yy.reshape(16)
+
+            patch_x.append(idx_x)
+            patch_y.append(idx_y)
+        # print(y, cnt)
+    
+    global PATCH_IDX
+    PATCH_IDX = [tuple(patch_y), tuple(patch_x)]
 
 def registration(img1, img2):
     sz = img1.shape
@@ -29,8 +48,6 @@ def _optical_flow(imgs):
 def optical_flow(imgs):
     """ Compute optical flow of t-1, t, t+1 images """
 
-    imgs = [cv2.cvtColor(x, cv2.COLOR_BGR2GRAY) for x in imgs]
-
     flows = []
     for i, _ in enumerate(imgs):
         if i > 0 and i < len(imgs) - 1:
@@ -38,26 +55,40 @@ def optical_flow(imgs):
             flow = _optical_flow([imgs[i-1],imgs[i],imgs[i+1]])
             # print(flow.shape)
 
-            for y in range(0,flow.shape[0]-STRIDE,STRIDE):
-                for x in range(0,flow.shape[1]-STRIDE,STRIDE):
-                    
-                    patch = flow[y:y+STRIDE,x:x+STRIDE]
-                    # print(np.linalg.norm(patch))
-                    if np.linalg.norm(patch) > THRESHOLD:
-                        flows.append(patch.flatten())
-            
-            # print(len(flows))
+            flowX_patch = flow[:, :, 0][tuple(PATCH_IDX)]
+            flowY_patch = flow[:, :, 1][tuple(PATCH_IDX)]
+            flow_patch = np.concatenate((flowX_patch, flowY_patch), axis=1)
+            print(flow_patch.shape)
+            valid_flow = flow_patch[np.linalg.norm(flow_patch, axis=1) > THRESHOLD]
+            print(valid_flow.shape)
 
-    flows = np.array(flows)
-    return flows
+    return valid_flow
 
-DICT = {}
+generate_patches()
 
 for i, folder in enumerate(sorted(os.listdir(DATA_PATH))):
     imgs = []
-    for files in sorted(os.listdir(os.path.join(DATA_PATH,folder))):
+    for j, files in enumerate(sorted(os.listdir(os.path.join(DATA_PATH,folder)))):
         # print("Reading ", os.path.join(DATA_PATH,folder,files))
-        imgs.append(cv2.imread(os.path.join(DATA_PATH,folder,files)))
+        img = cv2.imread(os.path.join(DATA_PATH,folder,files))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        resized = cv2.resize(gray,(552, 982))
+        imgs.append(resized)
+        
+    # print(imgs[0][0:16,0:16])
+    # ii = imgs[0][tuple(PATCH_IDX)]
+    # print(len(PATCH_IDX[1]))
+    # print(ii[0].reshape(4,4).T)
+    # print(ii[3].reshape(4,4).T)
+
+    # # print(ii[1305].reshape(4,4).T)
+    # # print(ii[1302].reshape(4,4).T)
+
+    # # print(ii[:4,:].reshape(4,4,4))
+    # # print(PATCH_IDX)
+    
+    
+    # break
     print("Folder ", folder, " in progress")
     DICT[folder] = {}
     # (A): the native direction of the video
@@ -89,10 +120,10 @@ for i, folder in enumerate(sorted(os.listdir(DATA_PATH))):
         flows = np.concatenate((flows, flow),axis=0)
 
     
-pickle_out = open("dict.pickle","wb")
-pickle.dump(DICT, pickle_out)
-pickle_out.close()
+# pickle_out = open("dict.pickle","wb")
+# pickle.dump(DICT, pickle_out)
+# pickle_out.close()
 
-pickle_out = open("patch_flow.pickle","wb")
-pickle.dump(flows, pickle_out)
-pickle_out.close()
+# pickle_out = open("patch_flow.pickle","wb")
+# pickle.dump(flows, pickle_out)
+# pickle_out.close()
